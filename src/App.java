5d25c2ef80c3525fa52189112477c1e883c16545
web-mazhar <mazharulislam.enlightsolutions@gmail.com>
2025-07-24 23:58:19 +0600

    added add and addfirst in linked list

--- src/App.java
+++ src/App.java
@@ -1,16 +1,21 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        curstomArray myArray = new curstomArray(1);
-        myArray.add(10);
-        myArray.add(20);
-        myArray.add(30);
-        myArray.add(40);
-        myArray.add(50);
-        myArray.add(60);
-        System.out.println("Reversed array: " + Arrays.toString(myArray.reverse()));
+        // curstomArray myArray = new curstomArray(1);
+        // myArray.add(10);
+        // myArray.add(20);
+        // myArray.add(30);
+        // myArray.add(40);
+        // myArray.add(50);
+        // myArray.add(60);
+        // System.out.println("Reversed array: " + Arrays.toString(myArray.reverse()));
+
+        // --------------Linked List --------------------//
+        customLinkedList linkedList = new customLinkedList();
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(30);
+        linkedList.addFirst(5);
     }
 }
---
+++ src/customLinkedList.java
@@ -0,0 +1,40 @@
+public class customLinkedList {
+
+    private Node head;
+    private Node tail;
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public void add(int value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+    }
+
+    public void addFirst(int value) {
+        Node node = new Node(value);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+    }
+
+}
